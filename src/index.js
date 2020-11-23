c31e14335b128a2890d6508c77e724b10fe57fa4
nedbrennan <nedbrnnn@gmail.com>
2020-11-23 12:31:20 -0500

    issues with browser map

---
+++ src/index.js
@@ -0,0 +1,14 @@
+console.log('Hello World from your main file!');
+
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken = "pk.eyJ1IjoibmVkYnJubm4iLCJhIjoiY2todXNjZTE0MGE5ZDJ6bW43cWFmOWQ5dyJ9.-dzWjtE6NdHjllJbpmQZgQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+
